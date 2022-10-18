e92e0a7bf47ef3e72b0e17f32c42faa7ed42f0ed
SANDEEP AKASH <113710218+sandeepakash@users.noreply.github.com>
2022-10-18 21:57:56 +0530

    Add files via upload

--- Learning/python/basics.py
+++ Learning/python/basics.py
@@ -1,5 +1,59 @@
 # Let's start with the basics of Python program
+
 # just diaplay the statement
+
+from pickle import TRUE
+
+
 print('Hello Guys')
 
-#
+#getting input from the user
+
+#int(input("enter any number : "))
+
+# declaring and accessing variable let get be the variable
+
+get='simple'
+print('python is ',get)
+
+# Let us initailizle the variables for our different data types
+
+num=10         #integer data type where to initialize int
+float=10.0     #float data type which uses decimal characteristics
+string='hello' #String variable whic contains alphabetic and other types
+print(string) 
+print(num+float)
+#print(int+string)         #where the operation cannot be done because we cannot covatenate the values of numeriacal and string together
+
+# Data Types numerical , Boolean ,sequence , mapping ,text types
+ #Numerical data types
+ 
+a=10  # integer data type
+b=10.0 #float data type
+c=24+4j #complex data type
+
+# string data type
+d='simple quote'
+
+#boolean data type
+e=TRUE
+
+# sequence data type
+f=(10,11,12,13,14) # tuple
+print(f)
+g=[1,23,3,4,5,6,7] # list
+print(g[1:3])
+
+# mapping data type
+h={1:'collect',2:'train',3:'test'} # Dictionary
+print(h)
+
+#Operators 
+
+#arithmetic operator
+#Logical operator
+#Assignment operators
+#Comparison operators
+#Identity operators
+#Membership operators
+#Bitwise operators
